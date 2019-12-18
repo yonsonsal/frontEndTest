2a2f2b5700cb56fa0892b7bd51c31149b6259380
yonsonsal <ignacio.talavera@gmail.com>
2019-12-18 09:38:53 -0300

    Components

---
+++ src/components/Item.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Item = ({ item }) => {
+  const picture = item.picture;
+  return (
+    <div className="item">
+      <h2>$ {item.price}</h2>
+      <div>
+        <img width="38px" alt={`Item: ${item.title}`} src={picture} />
+      </div>
+      <p>({item.title})</p>
+    </div>
+  );
+};
+
+export default Item;
